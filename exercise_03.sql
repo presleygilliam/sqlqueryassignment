dcf8ca17eb7e67e367eb4a6f77f93aa2e1ef567f
Presley Gilliam <72228192+presleygilliam@users.noreply.github.com>
2023-03-26 22:43:58 -0400

    Update exercise_03.sql

--- exercise_03.sql
+++ exercise_03.sql
@@ -1,3 +1,3 @@
-SELECT student.first_name AS 'Student FNAME', student.last_name AS 'Student LNAME', instructor.first_name AS 'Instructor FNAME', instructor.last_name AS 'Instructor LNAME'
+SELECT student.first_name AS 'Student firstName', student.last_name AS 'Student lastName', instructor.first_name AS 'Instructor firstName', instructor.last_name AS 'Instructor lastName'
     FROM student,instructor
     WHERE student.advisor_id = instructor.instructor_id;
